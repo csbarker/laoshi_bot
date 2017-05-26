f67f3937a7ad4f7bb84ceee48e66da8427272413
Callum Barker <callum.scott.barker@gmail.com>
2017-05-26 21:19:36 +0800

    game start and stop

--- app/game.js
+++ app/game.js
@@ -1,11 +1,78 @@
-module.exports = class Game {
-   constructor(client, params, msg) {
-       this.client = client;
-       this.params = params;
-	   this.msg = msg;
-   }
+var fs = require("fs");
 
-   setup() {
-       this.msg.reply('Now playing HSK'+params[1]);
-   }
+module.exports = class Game {
+	constructor(client) {
+		this.client = client;
+		this.valid_options = ['1','2','3','4','5','6']
+		this.games = {
+			//"HSK_Bot_Test<#317297114155843584>": 'true'
+		};
+
+		// load hsk strings
+		this.lang = {};
+
+		for (var i = 0; i <= this.valid_options.length - 1; i++) {
+			var key = this.valid_options[i];
+			this.lang[key] = fs
+				.readFileSync(`./lang/HSK_${key}.txt`)
+				.toString('utf-8')
+				.split('\n');
+		}
+	}
+
+	in_progress(guild, channel) {
+		return this.games.hasOwnProperty(guild + channel);
+	}
+
+	/*
+	*	START THE GAME
+	*/
+	init(params, msg) {
+		var hsk_level = params[1] || 1;
+		var characters = params[2] || this.lang[hsk_level].length;
+
+		// Validation
+		if (this.valid_options.indexOf(params[1]) === -1) {
+			msg.reply('please select a number from 1 to 6');
+			return;
+		}
+
+		var hsk_length = this.lang[hsk_level].length;
+		if (!Number.isInteger(characters) 
+			|| characters < hsk_length 
+			|| characters > hsk_length 
+			|| characters <= 0) {
+				msg.reply(`the amount must be between 1 and ${hsk_length}`)
+				return;
+		}
+
+		// All good; lets get ready to rumble
+		var game_key = msg.guild + msg.channel;
+		this.games[game_key] = {
+			round: 1,
+			hsk_level: hsk_level,
+			started: Date.now(),
+			rounds: {},
+			results: {}
+		}
+
+		msg
+			.reply(`Now practicing HSK ${params[1]} (${characters} characters)`)
+			.then(msg => this.start_round(msg, params));
+	}
+
+	/*
+	* END GAME
+	*/
+	end_game(msg) {
+		var game_key = msg.guild + msg.channel;
+		delete this.games[game_key];
+		msg.reply('has ended the game');
+	}
+
+	start_round(msg, params) {
+		var random_entry = this.lang[params[1]][30];
+		var random_data = random_entry.split('\t');
+		msg.reply(`Translate: ${random_data[0]} / ${random_data[0]}`);
+	}
 }
--- index.js
+++ index.js
@@ -3,24 +3,29 @@ const client = new Discord.Client();
 const config = require('./config');
 
 var Game = require('./app/game');
+var game = new Game(client);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
 
 client.on('message', msg => {
-	game_in_progress = false;
+	cmd = msg.content;
 
 	/* Commands */
-	if (msg.content.substr(0,4) === '!hsk') {
-		if (game_in_progress) {
-			msg.reply('A game is already in progress!');
+	if (cmd.substr(0,4) === '!hsk') {
+		if (game.in_progress(msg.guild, msg.channel)) {
+			if (cmd === '!hsk stop') {
+				game.end_game(msg);
+				return;
+			}
+
+			msg.reply('a game is already in progress! use !hsk stop');
 			return;
 		}
 	
 		params = msg.content.toLowerCase().split(" ");
-		game = new Game(client, params, msg);
-		game.setup();
+		game.init(params, msg);
 	}
 });
 
