f07f188486f742f09d0b08654582646565586ce0
Callum Barker <callum.scott.barker@gmail.com>
2017-05-27 19:10:10 +0800

    implemented points and game results

--- app/game.js
+++ app/game.js
@@ -6,12 +6,11 @@ module.exports = class Game {
 	constructor(client) {
 		this.client = client;
 		this.valid_options = ['1','2','3','4','5','6']
+		this.players = {};
+		this.lang = {};
 		this.games = {
 			//"HSK_Bot_Test<#317297114155843584>": 'true'
 		};
-
-		// load hsk strings
-		this.lang = {};
 
 		for (var i = 0; i <= this.valid_options.length - 1; i++) {
 			var key = this.valid_options[i];
@@ -31,7 +30,7 @@ module.exports = class Game {
 	*/
 	init(params, msg) {
 		var hsk_level = params[1] || 1;
-		var characters = parseInt(params[2]) || this.lang[hsk_level].length;
+		var characters = parseInt(params[2]) || 1; //this.lang[hsk_level].length;
 
 		// Validation
 		if (this.valid_options.indexOf(params[1]) === -1) {
@@ -104,12 +103,12 @@ module.exports = class Game {
 		};
 		this.games[game_key].characters_left.splice(character_key, 1);
 
-		console.log(character_english);
+		console.log(this.games[game_key].rounds[i].answers);
 
 		var _this = this;
 		setTimeout(function() {
 			_this.finish_round(i ,msg);
-		}, 5000);
+		}, 15000);
 	}
 
 	finish_round(i, msg) {
@@ -117,13 +116,16 @@ module.exports = class Game {
 		var _game = this.games[game_key] || null;
 		if (_game === null) return;
 
+		this.determine_round_results(i, msg);
+
 		// game over?
 		if (i == _game.total_rounds) {
+			this.output_game_results(msg);
 			this.end_game(msg);
 			return;
 		}
 
-		this.determine_round_results(i, msg);
+		//this.determine_round_results(i, msg);
 
 		// next round
 		this.games[game_key].round++;
@@ -137,10 +139,12 @@ module.exports = class Game {
 
 		var current_round = this.games[game_key].round;
 		if(_game.rounds[current_round].players_answers.hasOwnProperty(msg.author)) return;
+
 		_game.rounds[current_round].players_answers[msg.author] = {
 			name: msg.author,
 			answer: msg.content.toLowerCase(),
-			submitted: _.now()
+			submitted: _.now(),
+			correct: (_game.rounds[current_round].answers.indexOf(msg.content.toLowerCase()) !== -1)
 		};
 	}
 
@@ -150,8 +154,7 @@ module.exports = class Game {
 		var _round = _game.rounds[i] || null;
 		if (_game === null) return;
 
-		var compiled = _.template(":white_check_mark: <%= name %>: <%= answer %>\n");
-		var compile_question = _.template("");
+		var compiled = _.template("<%= icon %> <%= name %>: <%= points %>\n");
 		var player_results = '';
 		var next_round = i + 1;
 		var next_round_string = (i == _game.total_rounds) ? 'This was the last round.' : `Round #${next_round} will begin shortly; use !hsk stop to quit`;
@@ -161,6 +164,8 @@ module.exports = class Game {
 			player_results += ':zzz: no players this round';
 		} else {
 			_.each(_round.players_answers, function(data) {
+				data.icon = (data.correct) ? ':white_check_mark:' : ':white_large_square:';
+				data.points = (data.correct) ? '+10 points' : '';
 				player_results += compiled(data);
 			});
 		}
@@ -175,6 +180,61 @@ module.exports = class Game {
   			'Tools: ', 
   			`[Example Sentences](http://ce.linedict.com/dict.html#/cnen/example?query=${char_for_url})`
 			)
+  		.addField('Results: ', player_results);
+
+		msg.channel.send({embed: results_embed});
+	}
+
+	output_game_results(msg) {
+		var game_key = msg.guild + msg.channel;
+		var _game = this.games[game_key] || null;
+		if (_game === null) return;
+
+		// go through each round
+		var _self = this;
+		_.each(_game.rounds, function(round) {
+			if(Object.keys(round.players_answers).length <= 0) return; // no players
+			// and each rounds answers
+			_.each(round.players_answers, function(pa) {
+				// new player (glboal)
+				if(!_self.players.hasOwnProperty(pa.name)) {
+					_self.players[pa.name] = {
+						score: 0
+					}
+				}
+				// new player (local game)
+				if(!_game.results.hasOwnProperty(pa.name)) {
+					_game.results[pa.name] = {
+						name: pa.name,
+						score: 0
+					}
+				}
+
+
+				if (!pa.correct) return;
+
+				_game.results[pa.name].score += 10; // local
+				_self.players[pa.name].score += 10; // global
+			});
+		});
+
+		// now that we have updated the players scores we can output them
+		var row_template = _.template(":trophy: <%= name %>: +<%= score %> (<%= global_score %>)\n");
+		var player_results = '';
+		
+		if (Object.keys(_game.results).length <= 0) {
+			player_results += ':zzz: no points awarded this game :sweat_smile:';
+		} else {
+			_.each(_game.results, function(data) {
+				data.global_score = _self.players[data.name].score;
+				player_results += row_template(data);
+			});
+		}
+
+		var results_embed = new Discord.RichEmbed()
+			.setTitle(`HSK Practice Complete`)
+	  	.setColor('RED')
+	  	.setFooter('Use !hsk [level] [characters] to start a new game', 'http://i.imgur.com/w1vhFSR.png')
   		.addField('Results: ', player_results);
 
 		msg.channel.send({embed: results_embed});
